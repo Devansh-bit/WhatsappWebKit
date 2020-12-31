3520e3ea7d8c254ac5e035bd8a6af737f86ab582
Devansh-bit <67866743+Devansh-bit@users.noreply.github.com>
2020-12-31 21:00:39 +0530

    fixes

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='WhatsappWebKit',
+      version='1.1.5',
+      description='A small library helpful for manipulating whatsapp web using selenium',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Devansh-bit/WhatsappWebKit',
+      author='Devansh Gupta',
+      author_email='xyznfc@gmail.com',
+      license='MIT',
+      packages=['WhatsappWebKit'],
+      install_requires=[
+          'selenium',
+      ],
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ]
+)
+
